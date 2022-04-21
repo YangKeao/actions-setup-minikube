1b5625e4a871b88db0d18525dec3f9ba4205d01b
YangKeao <yangkeao@chunibyo.icu>
2022-04-21 20:33:28 +0800

    add arm64 support
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- src/download.js
+++ src/download.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const os = require('os');
+
 const core = require('@actions/core');
 const axios = require('axios');
 const tc = require('@actions/tool-cache');
 
 const isLinux = name => name.indexOf('linux') >= 0;
 const isAmd64 = name => name.indexOf('amd64') >= 0;
+const isArm64 = name => name.indexOf('arm64') >= 0;
 const isSignature = name => name.indexOf('sha256') >= 0;
 
 const download = async inputs => {
@@ -20,8 +23,16 @@ const download = async inputs => {
     url: tagInfoUrl,
     headers
   });
+  const arch = os.arch();
   const downloadUrl = tagInfo.data.assets.find(
-    asset => isLinux(asset.name) && isAmd64(asset.name) && !isSignature(asset.name)
+    asset => {
+      const linux_not_signature = isLinux(asset.name) && !isSignature(asset.name);
+      if (arch === "arm64") {
+        return linux_not_signature && isArm64(asset.name);
+      } else {
+        return linux_not_signature && isAmd64(asset.name);
+      }
+    }
   ).browser_download_url;
   core.info(`Minikube version found at: ${downloadUrl}`);
   return tc.downloadTool(downloadUrl);
